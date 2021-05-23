cdd64eeded08d1fbfef734b7b7903f17afbe1436
naodevtech <naimtahouri@gmail.com>
2021-05-23 17:51:19 +0200

    :fire: add unicode :fire:

--- src/components/molecules/category/category.js
+++ src/components/molecules/category/category.js
@@ -3,6 +3,7 @@ import "./_category.scss";
 function Category({ props }) {
   return (
     <div className="box_category">
+      <h2>{props.emoji}</h2>
       <h4>{props.name}</h4>
     </div>
   );
